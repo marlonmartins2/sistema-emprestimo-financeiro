4d7227392a997ed39dcbed3d91c726b2f6dee6f4
Marlon Martins <marlon.azevedo.m@gmail.com>
2023-06-15 15:31:26 -0300

    feat: add result on return to function.

--- back_end/core/tasks.py
+++ back_end/core/tasks.py
@@ -16,13 +16,21 @@ def process_proposals(self):
     Process all proposals with status "pending" and change the status to "accepted" or "declined" or "expired"
     """
     proposals = Proposal.objects.filter(status="pending")
+
+    results = []
+
     for proposal in proposals:
         try:
             proposal_time_limit = proposal.created_at + timedelta(minutes=5)
             if proposal_time_limit < timezone.now():
                 proposal.status = "expired"
                 proposal.save()
-                print(f"Proposal id: {proposal.id} - Status: {proposal.status}")
+
+                results.append({
+                    "proposal_id": proposal.id,
+                    "status": proposal.status
+                })
+
                 continue
 
             document_is_valid = cpf.validate(proposal.document)
@@ -30,14 +38,24 @@ def process_proposals(self):
             if document_is_valid and proposal.proposal_value >= 1000:
                 proposal.status = "accepted"
                 proposal.save()
-                print(f"Proposal id: {proposal.id} - Status: {proposal.status}")
+
+                results.append({
+                    "proposal_id": proposal.id,
+                    "status": proposal.status
+                })
+
                 continue
             else:
                 proposal.status = "declined"
                 proposal.save()
-                print(f"Proposal id: {proposal.id} - Status: {proposal.status}")
+
+                results.append({
+                    "proposal_id": proposal.id,
+                    "status": proposal.status
+                })
+
                 continue
         except Exception as e:
             print(e)
             continue
-    return True
+    return results
